738ca9bb97e847fd907a3ddd20ffaeca7a70679c
Mobileteam-admin <vishnuanil23392@gmail.com>
2026-02-23 22:56:49 +0530

    refactor: use super parameters in failures.dart

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -4,13 +4,13 @@ abstract class Failure {
 }
 
 class ServerFailure extends Failure {
-  const ServerFailure(String message) : super(message);
+  const ServerFailure(super.message);
 }
 
 class CacheFailure extends Failure {
-  const CacheFailure(String message) : super(message);
+  const CacheFailure(super.message);
 }
 
 class NetworkFailure extends Failure {
-  const NetworkFailure(String message) : super(message);
+  const NetworkFailure(super.message);
 }
